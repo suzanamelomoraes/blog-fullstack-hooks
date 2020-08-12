b2382917b1324618fa9d315d4c42dbdb7f73d2b2
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-08-12 18:46:18 +1200

    update component to manage the fake data by index to work on the frontend layout until receive the complete data, including id, from backend

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -3,13 +3,18 @@ import Post from './Post';
 
 const Blog = () => {
   const [posts, setPosts] = useState([
-    { id: 1, title: 'Post One', body: 'This is my first post' },
-    { id: 2, title: 'Post Two', body: 'This is my second post' },
-    { id: 3, title: 'Post Three', body: 'This is my third post' },
+    { title: 'Post One', body: 'This is my first post' },
+    { title: 'Post Two', body: 'This is my second post' },
+    { title: 'Post Three', body: 'This is my third post' },
   ]);
 
-  const post = posts.map((post) => (
-    <Post key={post.id} id={post.id} title={post.title} body={post.body} />
+  const post = posts.map((post, index) => (
+    <Post
+      key={post.index}
+      id={post.index}
+      title={post.title}
+      body={post.body}
+    />
   ));
 
   return <div>{post}</div>;
