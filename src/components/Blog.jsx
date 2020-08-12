44c70f2079f82e678b046511d3d60aeb744e56db
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-08-12 21:29:38 +1200

    include comment properties in the state, methods, props and return to be rendered when it exists

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -4,18 +4,24 @@ import AddPost from './AddPost';
 
 const Blog = () => {
   const [posts, setPosts] = useState([
-    { title: 'Post One', body: 'This is my first post' },
-    { title: 'Post Two', body: 'This is my second post' },
-    { title: 'Post Three', body: 'This is my third post' },
+    { title: 'Post One', body: 'This is my first post', comment: 'Loved it!' },
+    { title: 'Post Two', body: 'This is my second post', comment: '' },
+    { title: 'Post Three', body: 'This is my third post', comment: '' },
   ]);
 
-  const addPost = ({title, body}) => {
-    const newPosts = [...posts, { title, body }];
+  const addPost = ({ title, body, comment }) => {
+    const newPosts = [...posts, { title, body, comment }];
     setPosts(newPosts);
   };
 
   const post = posts.map((post, index) => (
-    <Post key={index} id={index} title={post.title} body={post.body} />
+    <Post
+      key={index}
+      id={index}
+      title={post.title}
+      body={post.body}
+      comment={post.comment}
+    />
   ));
 
   return (
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const Post = ({ id, title, body }) => {
+const Post = ({ id, title, body, comment }) => {
   return (
     <div>
       <h4>{title}</h4>
       <p>{body}</p>
+      {comment && <p>{comment}</p>}
     </div>
   );
 };
