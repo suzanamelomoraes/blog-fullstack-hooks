ecad8328bfd24d1274ee36ac6d675d51a3cc5197
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-08-12 17:37:18 +1200

    create fake posts data to work with while building the frontend and before receive the data from the backend

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 
 const Blog = () => {
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'Post One', body: 'This is my first post' },
+    { id: 2, title: 'Post Two', body: 'This is my second post' },
+    { id: 3, title: 'Post Three', body: 'This is my third post' },
+  ]);
   return <div>Post</div>;
 };
 
