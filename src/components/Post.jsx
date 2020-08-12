15093d4cb2853059d277be218f09f7cac3405f77
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-08-12 21:41:25 +1200

    create a button that calls the method removePost, sent by the Blog parent component as props and send the selected post id from it to the parent

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const Post = ({ id, title, body, comment }) => {
+const Post = ({ id, title, body, comment, removePost }) => {
   return (
     <div>
       <h4>{title}</h4>
       <p>{body}</p>
+      <button onClick={() => removePost(id)}>Delete post</button>
       {comment && <p>{comment}</p>}
     </div>
   );
