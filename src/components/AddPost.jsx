2c55240a55fa2d457c07ce1ab197bd6e03b79ebc
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-08-12 18:50:48 +1200

    create a form in a hook-form model which receive title and body data from the user as inputs, validade the inputs by making them required and display error message to the user when it is necessary, also handle the data submited by the user sending it to be added to the database by the parent by the method addPost received as props

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
-const AddPost = () => {
-  return <div></div>;
+const AddPost = ({ addTodo }) => {
+  const { register, handleSubmit, errors } = useForm();
+
+  const onSubmit = (data, e) => {
+    addTodo(data);
+    e.target.reset();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor='title'>Title</label>
+          <input
+            name='title'
+            placeholder='Add your title'
+            ref={register({ required: true })}
+          />
+          {errors.title && 'Title is required'}
+        </div>
+        <div>
+          <label htmlFor='Body'>Body</label>
+          <input
+            name='body'
+            placeholder='Tell your thoughts...'
+            ref={register({ required: true })}
+          />
+          {errors.body && 'Title is required'}
+        </div>
+        <input type='submit' />
+      </form>
+    </div>
+  );
 };
 
 export default AddPost;
