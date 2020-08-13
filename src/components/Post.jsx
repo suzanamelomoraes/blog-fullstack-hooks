d67875829d10751ea05ffdab8aeb0ea281b454b4
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-08-13 15:35:13 +1200

    create a state to receive an array of comments and another one to handle show addcomment form status. Create a method that receives the comment made by the user by AddComment form and set it to the comments state, also create a method to hadle show the form when the user clicks a button Add comment. Finally map the comments states to render it to the user and include a conditional to hadle the content of the state to be rendered amoung the posts they belong to

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AddComment from './AddComment';
 
-const Post = ({ id, title, body, comment, removePost }) => {
+const Post = ({ id, title, body, removePost }) => {
+  const [comments, setComments] = useState([
+    { comment: 'Love it', postId: 1 },
+    { comment: 'Amazing', postId: 1 },
+    { comment: 'Weel done', postId: 2 },
+  ]);
+  const [form, setShowForm] = useState({ showForm: false });
+
+  const addComment = ({ comment }) => {
+    const newComments = [...comments, { comment, postId: id }];
+    setComments(newComments);
+    const updateFormStatus = { ...form };
+    updateFormStatus.showForm = false;
+    setShowForm(updateFormStatus);
+  };
+
+  const showAddComment = () => {
+    const updateFormStatus = { ...form };
+    updateFormStatus.showForm = true;
+    setShowForm(updateFormStatus);
+  };
+
+  const showComments = comments.map(
+    (eachComment, index) =>
+      eachComment.postId === id && <p key={index}> {eachComment.comment} </p>
+  );
+
   return (
     <div>
       <h4>{title}</h4>
       <p>{body}</p>
       <button onClick={() => removePost(id)}>Delete post</button>
-      {comment && <p>{comment}</p>}
+      <button onClick={showAddComment}>Add comment</button>
+      {form.showForm && <AddComment addComment={addComment} />}
+
+      {showComments}
     </div>
   );
 };
