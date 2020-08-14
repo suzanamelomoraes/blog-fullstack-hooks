944568ba16ae37375356e202f9120c09c819eca4
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-08-14 17:47:57 +1200

    move all the data, state and methods relates to comments to its own component to be rendered in the post parent

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AddComment from './AddComment';
 
-const Comments = ({comment}) => {
-return <div>{comment}</div>;
+const Comments = ({ postId }) => {
+  const [comments, setComments] = useState([
+    { comment: 'Love it', postId: 1 },
+    { comment: 'Amazing', postId: 1 },
+    { comment: 'Weel done', postId: 2 },
+  ]);
+  const [form, setShowForm] = useState({ showForm: false });
+
+  const addComment = ({ comment }) => {
+    const newComments = [...comments, { comment, postId }];
+    setComments(newComments);
+    const updateFormStatus = { ...form };
+    updateFormStatus.showForm = false;
+    setShowForm(updateFormStatus);
+  };
+
+  const showAddComment = () => {
+    const updateFormStatus = { ...form };
+    updateFormStatus.showForm = true;
+    setShowForm(updateFormStatus);
+  };
+
+  const showComments = comments.map(
+    (eachComment, index) =>
+      eachComment.postId === postId && <p key={index}>{eachComment.comment}</p>
+  );
+
+  return (
+    <div>
+      <button onClick={showAddComment}>Add comment</button>
+      {form.showForm && <AddComment addComment={addComment} />}
+
+      {showComments}
+    </div>
+  );
 };
 
 export default Comments;
