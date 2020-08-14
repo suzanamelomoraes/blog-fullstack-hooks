e8283813b894a5b20542a8c59c2f64eb46f771bb
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-08-14 23:37:07 +1200

    create necessary files and require the necessary libraries to create midleware into app.js, also configure port

---
+++ Server/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const router = require('./route/blog');
+const server = express();
+
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(cors());
+server.use = router;
+
+const post = process.env.PORT || 3002;
+
+server.listen(port, () => {
+  console.log('Listening on port', port);
+});
