8c9b3d5ef69ad445ebdfcaf3430789c5bdc95f00
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-08-15 00:53:09 +1200

    create configuration and requests to get and add posts and comments from the database

---
+++ Server/route/blog.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const router = express.Router();
+const { pool } = require('../database/config');
+
+// GET all posts
+router.get('/posts', async (request, response) => {
+  pool.query('SELECT * FROM posts', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+});
+
+// GET posts by id
+router.get('/posts/:id', async (request, response) => {
+  const id = request.params.id;
+
+  pool.query('SELECT * FROM posts WHERE id=$1', [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+});
+
+// GET comments for a post
+router.get('/comments/:id', async (request, response) => {
+  const id = request.params.id;
+
+  pool.query(
+    'select * from comments where post_id = $1',
+    [id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).json(results.rows);
+    }
+  );
+});
+
+// POST a new post
+router.post('/addPost', async (request, response) => {
+  const { title, body } = request.body;
+  pool.query(
+    'INSERT INTO posts (title, body, date_created) VALUES ($1, $2, Now())',
+    [title, body],
+    (error) => {
+      if (error) {
+        throw error;
+      }
+      response.status(201).json({ status: 'success', message: 'Post added.' });
+    }
+  );
+});
+
+// POST a new comment
+router.post('/addComment', async (request, response) => {
+  const { post_id, comment } = request.body;
+  pool.query(
+    'INSERT INTO comments (post_id, comment, date_created) VALUES ($1, $2, Now())',
+    [post_id, comment],
+    (error) => {
+      if (error) {
+        throw error;
+      }
+      response
+        .status(201)
+        .json({ status: 'success', message: 'Comment added.' });
+    }
+  );
+});
+
+module.exports = router;
