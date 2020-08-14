3cade80e49635bb9e51040dacfdf659053416a50
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-08-14 20:54:47 +1200

    refactor component removing match and replacing it for useParams hook

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Comment from './Comments';
 import Axios from 'axios';
+import { useParams } from 'react-router-dom';
 
-const Post = ({ removePost, match }) => {
+const Post = ({ removePost }) => {
+  let { id } = useParams();
+
   useEffect(() => {
-    Axios.get(`http://localhost:3002/posts/${match.params.id}`)
+    Axios.get(`http://localhost:3002/posts/${id}`)
       .then(
         (res) => setPost(res.data[0])
         // console.log(res.data[0])
@@ -12,18 +15,16 @@ const Post = ({ removePost, match }) => {
       .catch((err) => {
         console.log(err);
       });
-  }, [match.params.id]);
+  }, [id]);
 
   const [post, setPost] = useState({});
-
-
 
   return (
     <div>
       <h4>{post.title}</h4>
       <p>{post.body}</p>
       <button onClick={() => removePost(post.id)}>Delete post</button>
-    <Comment postId={post.id} />
+      <Comment postId={id} />
     </div>
   );
 };
