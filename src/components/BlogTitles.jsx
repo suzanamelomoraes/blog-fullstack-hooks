becacfd9aae789656276ef80d8de6b309b82f7ff
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-08-14 16:36:24 +1200

    refactor component to receive, link to and display data based in the new information sent from the Blog parent, as the data is comming from an api. Remove index as it is not necessary

--- src/components/BlogTitles.jsx
+++ src/components/BlogTitles.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const BlogTitles = ({ posts }) => {
-  return (
-    <div>
-      {posts.map((post, index) => (
-        <h2 key={index}> <Link to={`post/${index}`} >{post.title}</Link> </h2>
-      ))}
-    </div>
-  );
+  const postTitles = posts.map((post) => (
+    <h2 key={post.id}>
+      {' '}
+      <Link to={`posts/${post.id}`}>{post.title}</Link>{' '}
+    </h2>
+  ));
+
+  return <div>{postTitles}</div>;
 };
 
 export default BlogTitles;
