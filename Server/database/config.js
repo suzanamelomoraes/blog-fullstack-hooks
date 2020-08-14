69a59b85312eb67ab322350c04145acb46775d26
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-08-14 23:46:54 +1200

    include configuration for database

---
+++ Server/database/config.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const { Pool } = require('pg');
+const isEnvironment = process.env.NODE_ENV === 'environment';
+
+const connection = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
+const pool = new Pool({
+  connection: isEnvironment ? process.env.DATABASE_URL : connection,
+  ssl: isEnvironment,
+});
+
+module.exports = pool;
