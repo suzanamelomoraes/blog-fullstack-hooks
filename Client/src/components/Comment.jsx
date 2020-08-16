f51139e45713c4358238617939c37f70e2e65dad
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-08-16 19:40:23 +1200

    import moment and apply it to format date received from the database

--- Client/src/components/Comment.jsx
+++ Client/src/components/Comment.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import moment from 'moment';
 
 const Comment = ({ comment, removeComment }) => {
   return (
     <div>
       {comment.comment}
+      <p>{moment(comment.date_created).format('MMM Do YY')}</p>
       <button onClick={() => removeComment(comment.id)}>Delete comment</button>
     </div>
   );
