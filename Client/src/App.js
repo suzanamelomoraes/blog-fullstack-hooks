34c523366ace579de47ba30be0b6dc534238110e
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-08-16 12:11:40 +1200

    import router and history and wrap the blog component sending history as props

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import Blog from './components/Blog';
 
+const history = createBrowserHistory();
+
 function App() {
+  
   return (
-    <div className='App'>
-      <Blog />
-    </div>
+    <Router history={history}>
+      <div className='App'>
+        <Blog />
+      </div>
+    </Router>
   );
 }
 
