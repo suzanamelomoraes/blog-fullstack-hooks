fc4a0721ebf2b11d3c852c5d7fd94bc1204059bd
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-08-18 02:39:23 +1200

    style the title links and apply list material-ui

--- Client/src/components/BlogTitles.jsx
+++ Client/src/components/BlogTitles.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  Paper,
+  List,
+  ListItemText,
+  Divider,
+} from '@material-ui/core';
+
+const useStyles = makeStyles({
+  link: {
+    textDecoration: 'none',
+  },
+});
 
 const BlogTitles = ({ posts }) => {
+  const classes = useStyles();
+
   const postTitles = posts.map((post) => (
-    <h2 key={post.id}>
+    <p key={post.id}>
       {' '}
-      <Link to={`/posts/${post.id}`}>{post.title}</Link>{' '}
-    </h2>
+      <Link className={classes.link} to={`/posts/${post.id}`}>
+        {post.title}
+      </Link>{' '}
+      <Divider />
+    </p>
   ));
 
-  return <div>{postTitles}</div>;
+  return (
+    <div>
+      <Paper elevation={0}>
+        <List>
+          <ListItemText primary={postTitles} />
+        </List>
+      </Paper>
+    </div>
+  );
 };
 
 export default BlogTitles;
