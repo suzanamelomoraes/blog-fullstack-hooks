f6bb23cc7d5cc35d36a7fda7f4b2b1942cac1065
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-08-18 02:45:50 +1200

    apply styling using material-ui features, such as grid, container and custimized button, also include addPost form inside a modal

--- Client/src/components/Blog.jsx
+++ Client/src/components/Blog.jsx
@@ -1,13 +1,58 @@
 import React, { useState, useEffect } from 'react';
 import { Route, useHistory } from 'react-router-dom';
-import { Modal, Button } from '@material-ui/core';
+import { Modal, Button, Container, Grid, Paper } from '@material-ui/core';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import axios from 'axios';
 import AddPost from './AddPost';
 import BlogTitles from './BlogTitles';
 import Post from './Post';
+import Header from './Header';
+
+const AddButton = withStyles({
+  root: {
+    boxShadow: 'none',
+    textTransform: 'none',
+    fontSize: 16,
+    padding: '6px 12px',
+    lineHeight: 1.5,
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:hover': {
+      boxShadow: 'none',
+    },
+    '&:active': {
+      boxShadow: 'none',
+    },
+    '&:focus': {
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
+    },
+  },
+})(Button);
+
+const useStyles = makeStyles((theme) => ({
+  blog: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  paper: {
+    padding: theme.spacing(2),
+    marginTop: 1,
+  },
+}));
 
 const Blog = () => {
   const history = useHistory();
+  const classes = useStyles();
 
   useEffect(() => {
     getPosts();
@@ -59,25 +104,41 @@ const Blog = () => {
   };
 
   return (
-    <div>
-      <div>
-        <Route path='/'>
-          <BlogTitles posts={posts} />
+    <>
+      <Container maxWidth='lg'>
+        <Header />
+        <Grid container className={classes.blog}>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>
+              <Route path='/'>
+                <Grid>
+                  <BlogTitles posts={posts} />
+                </Grid>
 
-          <Button variant='contained' color='primary' onClick={showAddPost}>
-            Add post
-          </Button>
-        </Route>
-        <Modal open={showForm} onClose={closeAddPost}>
-          <AddPost addPost={addPost} />
-        </Modal>
-      </div>
-      <div>
-        <Route path='/posts/:id'>
-          <Post removePost={removePost} />
-        </Route>
-      </div>
-    </div>
+                <Grid>
+                  <AddButton
+                    fullWidth
+                    variant='outlined'
+                    color='primary'
+                    onClick={showAddPost}
+                  >
+                    Add post
+                  </AddButton>
+                </Grid>
+              </Route>
+            </Paper>
+            <Modal open={showForm} onClose={closeAddPost}>
+              <AddPost addPost={addPost} />
+            </Modal>
+          </Grid>
+          <Grid item xs={9}>
+            <Route path='/posts/:id'>
+              <Post removePost={removePost} />
+            </Route>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
   );
 };
 
